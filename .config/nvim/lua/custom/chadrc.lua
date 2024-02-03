a9065da993fa47c14f463188596759045bbe32be
nithin-vunet <nithin@vunetsystems.com>
2024-02-03 17:45:12 +0530

    refactor: hide tabbar in neovim

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,7 +1,12 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'onedark' }
+M.ui = {
+  theme = 'mountain',
+  tabufline = {
+    enabled= false
+  }
+}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
